7e7f910e4c9601a1f280d9dc6e998419c664b386
Joao.Silva <jl95terceira@gmail.com>
2025-02-19 08:30:14 +0000

    switched rpc adapter methods

--- src/main/java/jl95/net/rpc/switched/TypedRequesterIf.java
+++ src/main/java/jl95/net/rpc/switched/TypedRequesterIf.java
@@ -9,8 +9,16 @@ public interface TypedRequesterIf<A, R> {
 
     RequesterIf<A, R> getFunction(String typeAlias);
 
-    default <A2, R2> TypedRequesterIf<A2, R2> adapted(Function1<A, A2> reqAdapter,
-                                                      Function1<R2, R> resAdapter) {
-        return typeAlias -> getFunction(typeAlias).adapted(reqAdapter, resAdapter);
+    default <A2, R2> TypedRequesterIf<A2, R2> adapted        (Function1<A, A2> requestAdapter,
+                                                              Function1<R2, R> responderAdapter) {
+        return typeAlias -> getFunction(typeAlias).adapted(requestAdapter, responderAdapter);
+    }
+    default <A2>     TypedRequesterIf<A2, R>  adaptedRequest (Function1<A, A2> requestAdapter) {
+
+        return adapted(requestAdapter, self::apply);
+    }
+    default <R2>     TypedRequesterIf<A, R2>  adaptedResponse(Function1<R2, R> responseAdapter) {
+
+        return adapted(self::apply, responseAdapter);
     }
 }
