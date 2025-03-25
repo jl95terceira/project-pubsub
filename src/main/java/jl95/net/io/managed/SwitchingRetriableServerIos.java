cc5053dc09ff8ed29b12063627b65ce6ade897a3
Joao.Silva <jl95terceira@gmail.com>
2025-03-25 09:12:38 +0000

    SwitchingRetriableServerIos is back in town

---
+++ src/main/java/jl95/net/io/managed/SwitchingRetriableServerIos.java
@@ -0,0 +1,27 @@
+package jl95.net.io.managed;
+
+import static jl95.lang.SuperPowers.I;
+
+import java.net.InetSocketAddress;
+
+import jl95.net.io.CloseableIos;
+import jl95.net.rpc.util.Util;
+
+public class SwitchingRetriableServerIos extends SwitchingRetriableIos {
+
+    public static SwitchingRetriableServerIos of(Iterable<InetSocketAddress> peerAddresses) {
+        return new SwitchingRetriableServerIos(peerAddresses);
+    }
+    public static SwitchingRetriableServerIos of(InetSocketAddress...        peerAddresses) {
+        return new SwitchingRetriableServerIos(I(peerAddresses));
+    }
+
+    @Override protected CloseableIos connect(InetSocketAddress addr) {
+        return Util.getIoAsServer(addr);
+    }
+
+    private SwitchingRetriableServerIos(Iterable<InetSocketAddress> peerAddresses) {
+
+        super(peerAddresses);
+    }
+}
