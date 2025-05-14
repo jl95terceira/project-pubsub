4045595b95963f3b333e5e6a268cd1531a83dbd6
Joao.Silva <jl95terceira@gmail.com>
2025-05-14 13:58:31 +0100

    publication: dedicated id

--- src/main/java/jl95/net/pubsub/protocol/Publication.java
+++ src/main/java/jl95/net/pubsub/protocol/Publication.java
@@ -2,6 +2,8 @@ package jl95.net.pubsub.protocol;
 
 import static jl95.lang.SuperPowers.*;
 
+import java.util.UUID;
+
 import javax.json.JsonValue;
 
 import jl95.lang.*;
@@ -9,12 +11,14 @@ import jl95.lang.variadic.*;
 
 public class Publication extends NamedDataClass {
 
-    public String topicName;
+    public UUID      id = UUID.randomUUID();
+    public String    topicName;
     public JsonValue data;
 
     @Override
     protected Iterable<Tuple2<String, ?>> namedData() {
         return I(
+            tuple("id"   , id),
             tuple("topic", topicName),
             tuple("data" , data)
         );
--- src/main/java/jl95/net/pubsub/util/serdes/PublicationJsonSerdes.java
+++ src/main/java/jl95/net/pubsub/util/serdes/PublicationJsonSerdes.java
@@ -2,6 +2,8 @@ package jl95.net.pubsub.util.serdes;
 
 import static jl95.lang.SuperPowers.*;
 
+import java.util.UUID;
+
 import javax.json.Json;
 import javax.json.JsonValue;
 
@@ -11,6 +13,7 @@ import jl95.net.pubsub.util.SerdesDefaults;
 public class PublicationJsonSerdes {
     public enum Id {
 
+        ID   ("id"),
         TOPIC("topic"),
         DATA ("data");
 
@@ -23,7 +26,8 @@ public class PublicationJsonSerdes {
         var job = Json.createObjectBuilder();
         for (var t: I(
 
-            tuple(Id.TOPIC, SerdesDefaults.stringToJson.call(req.topicName)),
+            tuple(Id.ID   , SerdesDefaults.stringToJson.apply(req.id.toString())),
+            tuple(Id.TOPIC, SerdesDefaults.stringToJson.apply(req.topicName)),
             tuple(Id.DATA , req.data)
 
         ).map(t -> tuple(t.a1.value, t.a2))) {
@@ -37,6 +41,7 @@ public class PublicationJsonSerdes {
         var x = new Publication();
         for (var t: I(
 
+            tuple(Id.ID   , method((String i) -> { x.id = UUID.fromString(SerdesDefaults.stringFromJson.apply(jo.get(i))); })),
             tuple(Id.TOPIC, method((String i) -> { x.topicName = SerdesDefaults.stringFromJson.apply(jo.get(i)); })),
             tuple(Id.DATA , method((String i) -> { x.data      = jo.get(i); }))
 
